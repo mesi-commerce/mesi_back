0e0c5810e3a765eb99f16ef2661e2fa9e0a54861
alix babin <alix.babin69@gmail.com>
2023-11-16 16:23:27 +0100

    initialisation projet spring

---
+++ src/test/java/com/example/mesi/MesiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
