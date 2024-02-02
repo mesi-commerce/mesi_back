1b59e500329c6dc8cb7ae09722a636115be7ee60
alix babin <alix.babin69@gmail.com>
2024-02-02 13:59:11 +0100

    addorigin dev

--- src/main/java/com/example/mesi/config/WebConfig.java
+++ src/main/java/com/example/mesi/config/WebConfig.java
@@ -22,6 +22,7 @@ public class WebConfig {
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:4200");
         config.addAllowedOrigin("http://127.0.0.1:4200");
+        config.addAllowedOrigin("https://preprod.novelstyle.store");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
