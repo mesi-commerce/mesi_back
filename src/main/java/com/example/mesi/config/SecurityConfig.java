4e222ca0b969f83f7f208bdeec963d46420e0385
alix babin <alix.babin69@gmail.com>
2024-02-19 10:40:57 +0100

    add requests matchers for api articles GET methode

--- src/main/java/com/example/mesi/config/SecurityConfig.java
+++ src/main/java/com/example/mesi/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
                 .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests((requests) ->
                         requests.requestMatchers(HttpMethod.POST, "/login", "/register").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/articles/*").permitAll()
                                 .anyRequest().authenticated()
                 );
         return http.build();
